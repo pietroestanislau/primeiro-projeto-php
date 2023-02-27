2714924d58111e3139cd10194873944f449c4ac9
pietroestanislau <126513102+pietroestanislau@users.noreply.github.com>
2023-02-27 16:45:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "ola mundo";
+        ?>
+    </h1>
+    <h2> 9+7=
+        <?php 
+        echo (9+7);
+        ?>
+    </h2>
+    <h2> 3+7=
+        <?php 
+        echo (3+7);
+        ?>
+    </h2>
+    <h2> 2+7=
+        <?php 
+        echo (2+7);
+        ?>
+    </h2>
+    <h2> 9+4=
+        <?php 
+        echo (9+4);
+        ?>
+    </h2>
+</body>
+</html>
